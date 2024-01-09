789cb628c8550f8cd6ed039b8dd68629fb6ff3d7
Serhii Ternovtsov <tserg42@gmail.com>
2024-01-10 00:12:08 +0200

    D1P2: Correct sum of calibration values

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,4 @@
-#[common::solution]
-fn calculate_solution(input: &str) -> usize {
+fn part1(input: &str) -> usize {
     input
         .lines()
         .map(|l| {
@@ -8,4 +7,41 @@ fn calculate_solution(input: &str) -> usize {
             format!("{first}{last}").parse::<usize>().unwrap()
         })
         .sum()
+}
+
+const NUMBERS: [&str; 18] = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+    "seven", "eight", "nine",
+];
+
+fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|l| {
+            let first = NUMBERS
+                .iter()
+                .enumerate()
+                .filter_map(|(i, n)| l.find(n).and_then(|p| Some((i, p))))
+                .min_by_key(|(_, p)| *p)
+                .unwrap()
+                .0;
+            let last = NUMBERS
+                .iter()
+                .enumerate()
+                .filter_map(|(i, n)| l.rfind(n).and_then(|p| Some((i, p))))
+                .max_by_key(|(_, p)| *p)
+                .unwrap()
+                .0;
+            let n = format!("{}{}", NUMBERS[first % 9], NUMBERS[last % 9])
+                .parse::<usize>()
+                .unwrap();
+            dbg!(&n);
+            n
+        })
+        .sum()
+}
+
+#[common::solution]
+fn calculate_solution(input: &str) -> (usize, usize) {
+    (part1(input), part2(input))
 }
